44836a02131374606ae054476afe3b5480a69772
shashank kullkarni <shashankkulkarni80425@gmail.com>
2021-08-05 01:17:51 +0530

    solved transaction error and can able to do now crud operation in indexed db

--- src/components/indexeddb2.vue
+++ src/components/indexeddb2.vue
@@ -13,12 +13,13 @@ export default {
    name:'indexedDb',
    data(){
       return{
-         db:""
+         db:"",
+         request:""
       }
    },
    methods:{
       createDb(){
-      //  window.indexedDB = window.indexedDB || window.mozIndexedDB || 
+      //   window.indexedDB = window.indexedDB || window.mozIndexedDB || 
       //    window.webkitIndexedDB || window.msIndexedDB;
 
           window.IDBTransaction = window.IDBTransaction || 
@@ -31,20 +32,20 @@ export default {
             window.alert("Your browser does not support a indexed Db");
          }
          const employeeData = [ 
-            { id: "01", name: "shashank", age: 23, email: "shashank@gmail.com" },
-            { id: "02", name: "utkarsh;", age: 32, email: "utk@gamil.com.com" }
+            { id: "00-01", name: "shashank", age: 23, email: "shashank@gmail.com" },
+            { id: "00-02", name: "utkarsh;", age: 32, email: "utk@gamil.com.com" }
          ];
-         var request = window.indexedDB.open("myDb",1);
-         request.onerror = function(){
+         this.request = window.indexedDB.open("myDb",1);
+         this.request.onerror = function(){
             console.log("error");
          }
-         request.onsuccess = function(){
-             this.db= request.result;
+         this.request.onsuccess = function(){
+             this.db= this.request.result;
            
             console.log("success: " +this.db);
          }
-         request.onupgradeneeded = function (event) {
-            var db1= event.target.result;
+         this.request.onupgradeneeded = function (event) {
+             var db1= event.target.result;
             var objectStore = db1.createObjectStore("employee",{keyPath: "id"});
             for(var i in employeeData){
                objectStore.add(employeeData[i]);
@@ -55,23 +56,48 @@ export default {
 
       },
       addData(){
-         var request = this.db.transcation(["employee"],"readwrite")
-         .objectStore("employee").add({id:"03", name:"kapil",age:"24",email:"kp@gmail.com"});
-         request.onsuccess =function(){
+         //  window.indexedDB = window.indexedDB || window.mozIndexedDB || 
+         // window.webkitIndexedDB || window.msIndexedDB;
+
+
+         window.IDBTransaction = window.IDBTransaction || 
+         window.webkitIDBTransaction || window.msIDBTransaction;
+
+         
+ window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+         window.msIDBKeyRange
+         // this.request = window.indexedDB.open("myDb",1);
+        this.db = this.request.result;
+        this.request = this.db.transaction(["employee"],"readwrite")
+         .objectStore("employee").add({id:"00-03", name:"kapil",age:24,email:"kp@gmail.com"});
+         this.request.onsuccess =function(){
             alert("kapil has been added");
          };
-         request.onerror-function() {
+         this.request.onerror-function() {
             alert("already exist")
             
          }
 
          alert("add method");
       },
+
+
+      
       readAll(){
+          window.IDBTransaction = window.IDBTransaction || 
+         window.webkitIDBTransaction || window.msIDBTransaction;
+
          
-         var transcation = this.db.tansaction("employee");
-         var objectStore = transcation.objectStore("employee");
-         objectStore.openCursor().onsuccess = function(event){
+ window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+         window.msIDBKeyRange
+
+         // this.db = this.request.result;
+         // var transcation = this.db.tansaction("employee");
+         // var objectStore = this.db.tansaction("employee").objectStore("employee");
+         this.request = this.db.transaction(["employee"],"readwrite")
+         .objectStore("employee").openCursor();
+         this.request.onsuccess = function(event){
+            console.log(event);
             var cursor = event.target.result;
             if(cursor){
                alert("Name for id " + cursor.key + "is" + cursor.value.name+ "Age:" + cursor.value.age + "email:" + cursor.value.email);
@@ -81,28 +107,57 @@ export default {
          
          alert("Read All Method")
       },
-      read(){
-         var transcation= this.db.transcation(['employee']);
-         var objectStore = transcation.objectStore("employee");
-         var request = objectStore.get("02");
 
-         request.onerror = function(){
+
+
+
+      read(){
+          window.IDBTransaction = window.IDBTransaction || 
+         window.webkitIDBTransaction || window.msIDBTransaction;
+
+         
+ window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+         window.msIDBKeyRange
+
+         //  this.db = this.request.result;
+         // var transcation= this.db.transcation(["employee"]);
+         this.request = this.db.transaction(["employee"])
+         .objectStore("employee").get("00-03");
+         // this.request = objectStore.get("00-03");
+
+         this.request.onerror = function(){
             alert("unable to retrive data from database");
          }
-         request.onsuccess = function() {
-            if(request.result){
-               alert( "name" + request.result.name+ "Age:" + request.result.age + "email:" + request.result.email);
+         this.request.onsuccess = function(event) {
+            console.log(event.target.result.name);
+            if(event.target.result){
+               alert( "name" + event.target.result.name + " Age:" + event.target.result.age + " email:" + event.target.result.email);
 
             }
+            else {
+                  alert("Kapil couldn't be found in your database!");
+               }
             
          }
 
       },
-      remove(){
-         var request = this.db.transcation(["employee"],"readwrite")
-         .objectStore("employee").delete("03");
 
-         request.onsuccess = function(){
+
+
+      remove(){
+            window.IDBTransaction = window.IDBTransaction || 
+         window.webkitIDBTransaction || window.msIDBTransaction;
+
+         
+ window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+         window.msIDBKeyRange
+
+
+         // this.db = this.request.result;
+          this.request = this.db.transaction(["employee"],"readwrite")
+         .objectStore("employee").delete("00-03");
+
+         this.request.onsuccess = function(){
             alert("kapil has removed from database");
          }
       }
